47c1ebf4d06b48cca1f160f713036330664b5763
Bystrov Aleksandr <a.bystrov@rambler-co.ru>
2016-11-29 23:42:45 +0300

    fix

---
+++ assets/js/application.js
@@ -0,0 +1,2 @@
+webpackJsonp([1],{0:function(n,t,u){"use strict";function e(n){return n&&n.__esModule?n:{"default":n}}u(10);var c=u(6);e(c)},10:function(n,t){}});
+//# sourceMappingURL=application.js.map
